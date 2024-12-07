db676e9b34b2b80f3d97e0ca679563dc781438f7
Viktor Gagach <viktor.gagach@gmail.com>
2024-12-07 05:49:26 -0800

    devnet files are added

--- ping.py
+++ ping.py
@@ -5,4 +5,8 @@ net_base = "192.168.1."
 for n in range(1,25):
     print(f"ping {net_base}{n}")
 
-    
+with open("ospf.cfg","w") as f:
+    for ospf_id in range(1,3):
+        f.write(f"router ospf {ospf_id}\n")
+        for n in range(1,5):
+            f.write(f"network {net_base}{n}\n")    
