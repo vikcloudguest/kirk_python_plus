1c87feadcc99bb15cb31c008e8326c26c682657d
Viktor Gagach <viktor.gagach@gmail.com>
2024-12-07 13:03:11 -0800

    network range 1,6 updated

--- ping.py
+++ ping.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-for i in range (1,5):
+for i in range (1,6):
     print(f'ping 192.168.1.{i}')
 net_base = "192.168.1."
 for n in range(1,6):
     print(f"ping {net_base}{n}")
 
 with open("ospf.cfg", 'w') as f:
-    for ospf_id in range(1,5):
+    for ospf_id in range(1,6):
         f.write(f"router ospf {ospf_id}\n")
         f.write(f"network {net_base}{n}\n")    
