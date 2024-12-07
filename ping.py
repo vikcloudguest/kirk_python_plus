7e893c254babaeded823f666a0e002483a608a76
Viktor Gagach <viktor.gagach@gmail.com>
2024-12-07 06:00:55 -0800

    OSPF is added

--- ping.py
+++ ping.py
@@ -2,7 +2,10 @@
 for i in range (1,5):
     print(f'ping 192.168.1.{i}')
 net_base = "192.168.1."
-for n in range(1,25):
+for n in range(1,6):
     print(f"ping {net_base}{n}")
 
-    
+with open("ospf.cfg", 'w') as f:
+    for ospf_id in range(1,3):
+        f.write(f"router ospf {ospf_id}\n")
+        f.write(f"network {net_base}{n}\n")    
