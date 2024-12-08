d71e1aaafa039f1e9c8e016c7545e7d7cf337e69
Viktor Gagach <viktor.gagach@gmail.com>
2024-12-08 08:13:21 -0800

    class_1 added

---
+++ class_1/net_lab_connect.py
@@ -0,0 +1,15 @@
+from netmiko import ConnectHandler
+import getpass
+ 
+ios_xe_routers = ["cisco3.lasthop.io", "cisco4.lasthop.io"]
+ios_xe_credentials = {}
+
+arista_routers = ["arista1.lasthop.io", "arista2.lasthop.io", "arista3.lasthop.io","arista4.lasthop.io"]
+arista_credentials = {}
+
+juniper_routers = []
+juniper_credentials = {}
+
+
+nexus_switches = []
+nexus_credentials = {}
