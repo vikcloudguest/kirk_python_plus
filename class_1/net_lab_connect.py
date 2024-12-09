6db976472e00f29c4858392f1dbbd5c55dffa857
Viktor Gagach <viktor.gagach@gmail.com>
2024-12-09 10:04:28 -0800

    main files are added

--- class_1/net_lab_connect.py
+++ class_1/net_lab_connect.py
@@ -1,8 +1,18 @@
 from netmiko import ConnectHandler
-import getpass
- 
+from getpass import getpass
+print("Input pass :")
+passw = input()
+
 ios_xe_routers = ["cisco3.lasthop.io", "cisco4.lasthop.io"]
-ios_xe_credentials = {}
+
+for ip in ios_xe_routers:
+
+    ios_xe_credentials = {"host": ip, "username": "pyclass", "password": passw, "device_type": "cisco_xe", "session_log": (f"{ip}.log")}
+    net_connect = ConnectHandler(**ios_xe_credentials)
+    print(f"we are connected to {net_connect.find_prompt()}")
+    #print(net_connect.find_prompt())
+    show_ver = net_connect.send_command("show ip int br")
+    print(show_ver)
 
 arista_routers = ["arista1.lasthop.io", "arista2.lasthop.io", "arista3.lasthop.io","arista4.lasthop.io"]
 arista_credentials = {}
